8635f113bc677b562ecb5d636bcb3447a6639226
yashas2419 <154054119+yashas2419@users.noreply.github.com>
2024-08-03 08:31:51 +0530

    Add files via upload

---
+++ py6.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pandas as pd
+import csv
+from pgmpy.estimators import MaximumLikelihoodEstimator
+from pgmpy.models import BayesianNetwork
+from pgmpy.inference import VariableElimination
+heartDisease = pd.read_csv('heart.csv')
+heartDisease = heartDisease.replace('?',np.nan)
+#print('Sample instances from the dataset are given below')
+#print(heartDisease.head())
+#print('\n Attributes and datatypes')
+#print(heartDisease.dtypes)
+model=BayesianNetwork([('age','heartdisease'),('sex','heartdisease'),('exang','heartdisease'),
+('cp','heartdisease'),('heartdisease','restecg'),('heartdisease','chol')])
+print('\nLearning CPD using Maximum likelihood estimators')
+model.fit(heartDisease,estimator=MaximumLikelihoodEstimator)
+print('\n Inferencing with Bayesian Network:')
+HeartDiseasetest_infer = VariableElimination(model)
+print('\n 1. Probability of HeartDisease given evidence= restecg')
+q1=HeartDiseasetest_infer.query(variables=['heartdisease'],evidence={'restecg':1})
+print(q1)
+print('\n 2. Probability of HeartDisease given evidence= cp ')
+q2=HeartDiseasetest_infer.query(variables=['heartdisease'],evidence={'cp':2})
+print(q2)
+
+
+#program 7
+
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.cluster import KMeans
+import sklearn.metrics as sm
+import pandas as pd
+import numpy as np
+iris = datasets.load_iris()
+X = pd.DataFrame(iris.data)
+X.columns = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width']
+y = pd.DataFrame(iris.target)
+y.columns = ['Targets']
+model = KMeans(n_clusters=3)
+model.fit(X)
+# Plot the Original Classifications
+plt.figure(figsize=(8,8))
+colormap = np.array(['red', 'lime', 'black'])
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[y.Targets], s=40)
+plt.title('Real Classification')
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
+
+# Plot the Models Classifications
+plt.figure(figsize=(8,8))
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[model.labels_], s=40)
+plt.title('K Mean Classification')
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
+print('The accuracy score of K-Mean: ',sm.accuracy_score(y, model.labels_))
+print('The Confusion matrixof K-Mean: ',sm.confusion_matrix(y, model.labels_))
+from sklearn import preprocessing
+scaler = preprocessing.StandardScaler()
+scaler.fit(X)
+xsa = scaler.transform(X)
+xs = pd.DataFrame(xsa, columns = X.columns)
+from sklearn.mixture import GaussianMixture
+gmm = GaussianMixture(n_components=3)
+gmm.fit(xs)
+y_gmm = gmm.predict(xs)
+# Plot the GMM Classification
+plt.figure(figsize=(8,8))
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[y_gmm], s=40)
+plt.title('GMM Classification')
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
+plt.show()
+print('The accuracy score of EM: ',sm.accuracy_score(y, y_gmm))
+print('The Confusion matrix of EM: ',sm.confusion_matrix(y, y_gmm))
+
+
+#program 8
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn import datasets
+iris=datasets.load_iris()
+x = iris.data
+y = iris.target
+"""
+print ('sepal-length', 'sepal-width', 'petal-length', 'petal-width')
+print(x)
+print('class: 0-Iris-Setosa, 1- Iris-Versicolour, 2- Iris-Virginica')
+print(y)
+"""
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
+#To Training the model and Nearest nighbors K=5
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(x_train, y_train)
+#To make predictions on our test data
+y_pred=classifier.predict(x_test)
+print('Confusion Matrix')
+print(confusion_matrix(y_test,y_pred))
+print('Accuracy Metrics')
+print(classification_report(y_test,y_pred))
+
+#program 10
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+# Load the iris dataset
+iris = datasets.load_iris()
+X = iris.data[:, :2] 
+y = iris.target
+# Splitting the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+test_size=0.3, random_state=42)
+# Feature scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+# Training the SVM model
+svm_classifier = SVC(kernel='linear', random_state=42)
+svm_classifier.fit(X_train, y_train)
+# Predicting the test set results
+y_pred = svm_classifier.predict(X_test)
+# Calculating the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+# Visualizing the decision boundary
+def plot_decision_boundary(classifier, X, y):
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+    np.arange(y_min, y_max, 0.1))
+    Z = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, alpha=0.4)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=20, edgecolors='k')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.title('SVM Decision Boundary')
+    plt.show()
+    # Plotting decision boundary
+    plot_decision_boundary(svm_classifier, X_train, y_train)
